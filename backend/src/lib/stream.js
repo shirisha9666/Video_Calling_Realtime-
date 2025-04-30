1192b03831c9430be63b6f8cd2cbff2f5bc4b5fe
shirisha9666 <kommushirisha9666@gmail.com>
2025-04-30 16:11:38 +0530

    StreamSetup

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,22 @@
+import {StreamChat} from "stream-chat";
+import "dotenv/config"
+const apiKey=process.env.STEAM_API_KEY;
+const apiSecret=process.env.STEAM_API_SECRET
+
+
+if(!apiKey || !apiSecret){
+    console.error("Stream APi key or Secret is Missing")
+}
+const streamClient=StreamChat.getInstance(apiKey,apiSecret)
+
+export const upsertStreamUser=async(userData)=>{
+    try {
+        await streamClient.upsertUsers([userData])
+        return userData
+    } catch (error) {
+       console.log("Error upserting Stream user :",error) 
+    }
+}
+// todo:do it later
+
+export const generateStreamToken=(userId)=>{};
