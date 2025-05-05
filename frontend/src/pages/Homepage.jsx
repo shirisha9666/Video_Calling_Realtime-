4567bad9e09a2cd58ec0347a4ea851c89df016ea
shirisha9666 <kommushirisha9666@gmail.com>
2025-05-05 16:27:17 +0530

    setup-client

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import toast from 'react-hot-toast'
+
+const Homepage = () => {
+  return (
+    <div>
+      <button onClick={()=>toast.error("hello world")}>click</button>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ frontend/src/pages/OnboaringPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OnboaringPage = () => {
+  return (
+    <div>OnboaringPage</div>
+  )
+}
+
+export default OnboaringPage
