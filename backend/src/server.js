7d6838e7351f41f7c0e545a2e2677c76a0131c6d
shirisha9666 <kommushirisha9666@gmail.com>
2025-05-06 15:28:24 +0530

    setup-tanstack-and-axiosInstance

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,17 @@ import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
 import { connectDb } from "./lib/Db.js";
 import cookieParser from "cookie-parser"
+import cors from "cors"
 dontenv.config()
 
 const app=express()
 const PORT=process.env.PORT || 5001
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true
+}))
 app.use("/api/auth",authRoutes)
 app.use("/api/user",userRoutes)
 app.use("/api/chat",chatRoutes)
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 import toast from 'react-hot-toast'
+import axios from "axios"
+import { axiosInstance } from '../lib/axios'
 
 const Homepage = () => {
+  const {data,isLoading,error}=useQuery({
+    queryKey:["todos"],
+    queryFn:async()=>{
+      const res=await axiosInstance.get("/auth/me")
+  return res.data
+  
+    }
+  })
+  console.log("result",data)
   return (
     <div>
       <button onClick={()=>toast.error("hello world")}>click</button>
